db2decfffb904d6980f7ee484ddaf2e2d5728923
Ned Flanderzz <devinechinmere@gmail.com>
2023-09-25 20:18:02 -0400

    fixed jwt secret key not being long enough and added Api response

---
+++ webSocketAPI/src/main/java/com/chatApp/webSocketAPI/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.chatApp.webSocketAPI.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+
+}
